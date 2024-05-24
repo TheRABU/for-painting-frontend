5347417681ad504f947c280114c559d61be88cd8
TheRABU <fazlerabbi114@gmail.com>
2024-05-24 15:41:49 +0600

    react typewriter package used in banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,4 +1,16 @@
+import { Typewriter, useTypewriter, Cursor } from "react-simple-typewriter";
 const Banner = () => {
+  const [text] = useTypewriter({
+    words: [
+      "Welcome to",
+      "Painting Express!",
+      "Meet your favorite art collection",
+    ],
+    loop: 5,
+    onLoopDone: () => {
+      console.log("loop done");
+    },
+  });
   return (
     // <section className="bg-gray-50">
     //   <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
@@ -38,8 +50,10 @@ const Banner = () => {
       <div className="dark:bg-violet-600">
         <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-50">
           <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-50">
-            Provident blanditiis cum exercitationem
+            {text}
+            <Cursor cursorColor="blue" />
           </h1>
+
           <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-50">
             Cupiditate minima voluptate temporibus quia? Architecto beatae esse
             ab amet vero eaque explicabo!
