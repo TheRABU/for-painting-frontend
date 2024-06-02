50127b6c8e9c4d6fd7f6736b47940a18529acc94
TheRABU <fazlerabbi114@gmail.com>
2024-06-02 20:21:40 +0600

    changed the user photo conditional rendering

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -147,19 +147,20 @@ const NavBar = () => {
             </div>
           </div>
         ) : (
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar tooltip"
-            data-tip="please login"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
-          </div>
+          // <div
+          //   tabIndex={0}
+          //   role="button"
+          //   className="btn btn-ghost btn-circle avatar tooltip"
+          //   data-tip="please login"
+          // >
+          //   <div className="w-10 rounded-full">
+          //     <img
+          //       alt="Tailwind CSS Navbar component"
+          //       src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+          //     />
+          //   </div>
+          // </div>
+          <span></span>
         )}
         {/* User Update Profile */}
         {/* {user && (
